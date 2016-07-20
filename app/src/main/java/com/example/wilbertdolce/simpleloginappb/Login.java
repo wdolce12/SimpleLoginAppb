dedbe2fe840e969306fca06d6f703ad9e3059133
wdolce12 <wdolce12@gmail.com>
2016-07-19 22:10:52 -0400

    7/19/16 10:10pm

--- app/src/main/java/com/example/wilbertdolce/simpleloginappb/Login.java
+++ app/src/main/java/com/example/wilbertdolce/simpleloginappb/Login.java
@@ -94,6 +94,8 @@ public class Login extends AppCompatActivity {
             }
         }
 
+        
+
         else
         {
             Toast.makeText(getApplicationContext(),"No Paired Bluetooth Devices Found",Toast.LENGTH_LONG).show();
